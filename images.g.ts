3545a5072f15fc7382605d7e1275a9e59230c435
kcaj007 <79746680+kcaj007@users.noreply.github.com>
2021-02-27 10:20:12 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 6 6 6 6 6 6 6 6 . . . . 
+. . . 6 9 6 6 6 6 6 6 c 6 . . . 
+. . 6 c 9 6 6 6 6 6 6 c c 6 . . 
+. 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
+. 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
+. 6 6 8 b b 8 b b b 8 8 b 9 6 6 
+. 6 8 b b b 8 b b b b 8 6 6 6 6 
+. 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
+. 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
+. 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
+. 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
+. 8 f f f f 8 8 8 8 f f f 8 8 8 
+. . f f f f f 8 8 f f f f f 8 . 
+. . . f f f . . . . f f f f . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 6 6 6 6 6 6 6 6 . . . . 
+. . . 6 9 6 6 6 6 6 6 c 6 . . . 
+. . 6 c 9 6 6 6 6 6 6 c c 6 . . 
+. 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
+. 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
+. 6 6 8 b b 8 b b b 8 8 b 9 6 6 
+. 6 8 b b b 8 b b b b 8 6 6 6 6 
+. 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
+. 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
+. 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
+. 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
+. 8 f f f f 8 8 8 8 f f f 8 8 8 
+. . f f f f f 8 8 f f f f f 8 . 
+. . . f f f . . . . f f f f . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,35 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -200
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterEast0, function (sprite, location) {
+    game.over(true)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(8)
+tiles.setTilemap(tilemap`level2`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 6 6 6 6 6 6 6 6 . . . . 
+    . . . 6 9 6 6 6 6 6 6 c 6 . . . 
+    . . 6 c 9 6 6 6 6 6 6 c c 6 . . 
+    . 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
+    . 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
+    . 6 6 8 b b 8 b b b 8 8 b 9 6 6 
+    . 6 8 b b b 8 b b b b 8 6 6 6 6 
+    . 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
+    . 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
+    . 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
+    . 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
+    . 8 f f f f 8 8 8 8 f f f 8 8 8 
+    . . f f f f f 8 8 f f f f f 8 . 
+    . . . f f f . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.ay = 400
+mySprite.vx = 100
+scene.cameraFollowSprite(mySprite)
